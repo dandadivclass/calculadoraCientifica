3ca50a14cd38988f2c2c3b367d6366e279672eeb
dandaraCarvalho <dandaracarvalhoestudos@gmail.com>
2025-05-05 00:02:02 -0300

    feat: adiciona o arquivo que contém as variáveis globais utilizadas na calculadora

---
+++ scripts/variaveisGlobais.js
@@ -0,0 +1,14 @@
+export const elementoVisor = document.getElementById('visor');
+export const painelFuncoes = document.getElementById('painelFuncoes');
+export const painelBotoes = document.getElementById('painelBotoes');
+
+export const funcoes = ['sqrt', 'sin', 'cos', 'tan'];
+export const teclasCalculadora = ['C', '(', ')', '/', '7', '8', '9', '*', '4', '5', '6', '-', '1', '2', '3', '+', '%', '^', '!', '='];
+export const atalhoFuncoes = {
+    r: 'sqrt(',
+    s: 'sin(',
+    c: 'cos(',
+    t: 'tan('
+};
+
+export let resultadoExibido = false;
